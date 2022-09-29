ba2533dfc5cbdb18fe36aecd02b0b8aec1a415de
Aka5h23 <86167036+Aka5h23@users.noreply.github.com>
2022-09-29 21:09:22 +0600

    Added new activities and buttons.
    2 New activities and buttons with intent named statictabactivity and dynamictabactivity.

--- app/src/main/java/com/example/tabview2/MainActivity.java
+++ app/src/main/java/com/example/tabview2/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.tabview2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.example.tabview2.DynamicTabActivity;
+import com.example.tabview2.StaticTabActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        button1 = findViewById(R.id.btn1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StaticTabActivity.class));
+            }
+        });
+
+        button2 = findViewById(R.id.btn2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DynamicTabActivity.class));
+            }
+        });
     }
 }
