5c903c7c57319734d4c9ae5d00e8f68676fc2f44
Aka5h23 <86167036+Aka5h23@users.noreply.github.com>
2022-09-29 21:19:42 +0600

    Static Tab
    Static Tab activity with scrollable tabs that can be viewed if clicked upon.

--- app/src/main/java/com/example/tabview2/StaticTabActivity.java
+++ app/src/main/java/com/example/tabview2/StaticTabActivity.java
@@ -1,14 +1,29 @@
 package com.example.tabview2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class StaticTabActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_static_tab);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //toolbar=findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
     }
+
 }
